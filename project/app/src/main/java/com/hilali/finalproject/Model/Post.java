1f8f00bfa3b67db195acb09733fbb910102a6f19
YaelSabag <yaelsabag304@gmail.com>
2021-02-04 20:16:46 +0200

    try 2

--- project/app/src/main/java/com/hilali/finalproject/Model/Post.java
+++ project/app/src/main/java/com/hilali/finalproject/Model/Post.java
@@ -4,7 +4,6 @@ package com.hilali.finalproject.Model;
 import java.time.LocalDate;
 
 public class Post {
-    private String yoo;
     private String pid;
     private String userID;
     private String title;
